e7c0fa4902f6ae53a7a983dc016f68562a4fd2cb
Patrick Coulombe <pcoulombe@tobostudio.com>
2019-02-21 15:54:17 -0500

    open market if app not found

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1225,6 +1225,9 @@ public class InAppBrowser extends CordovaPlugin {
                     override = true;
                 } catch (android.content.ActivityNotFoundException e) {
                     LOG.e(LOG_TAG, "Error with " + url + ": " + e.toString());
+                    Intent marketIntent = new Intent(Intent.ACTION_VIEW);
+                    marketIntent.setData(Uri.parse("market://details?id=com.groupebayard.lacroix"));
+                    startActivity(marketIntent);
                 }
             } else if (url.startsWith(WebView.SCHEME_TEL)) {
                 try {
